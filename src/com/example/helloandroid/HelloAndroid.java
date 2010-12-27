fa5916aab5d0c96f7b54cf300a973a73890cd537
Andi Sidwell <andi@takkaria.org>
2010-12-27 01:49:55 +0000

    We're a little bit further along, but not much...

--- src/com/example/helloandroid/HelloAndroid.java
+++ src/com/example/helloandroid/HelloAndroid.java
@@ -2,6 +2,8 @@ package com.example.helloandroid;
 
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -13,7 +15,8 @@ import java.lang.Runnable;
 
 public class HelloAndroid extends Activity
 {
-//	private Handler mHandler = new Handler();
+	private Handler mHandler = new Handler();
+	private TransitionDrawable mBackground;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -21,24 +24,28 @@ public class HelloAndroid extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-//		Button button = (Button) findViewById(R.id.reset);
-//		button.setOnClickListener(mResetListener);
+		Button button = (Button) findViewById(R.id.reset);
+		button.setOnClickListener(mResetListener);
+
+		mBackground = (TransitionDrawable) getResources().getDrawable(R.drawable.reset);
+//		mBackground.setCrossFadeEnabled(true);
 	}
 
 	private OnClickListener mResetListener = new OnClickListener() {
 		public void onClick(View v) {
-			// set up an alarm
-			Calendar cal = Calendar.getInstance();
-			cal.setTimeInMillis(System.currentTimeMillis());
-			cal.add(Calendar.SECOND, 60);
+			Button button = (Button) findViewById(R.id.reset);			
+			button.setText("Reset");
 
-//			mHandler.postAtTime(mTriggerTimer, cal.getTimeInMillis());
+			mHandler.removeCallbacks(mTriggerTimer);
+			mHandler.postDelayed(mTriggerTimer, 5000);
 		}
 	};
 
 	private Runnable mTriggerTimer = new Runnable() {
 		public void run() {
-			
+			Button button = (Button) findViewById(R.id.reset);			
+			mBackground.startTransition(500);
+			button.setText("Finished");
 		}
 	};
 }
